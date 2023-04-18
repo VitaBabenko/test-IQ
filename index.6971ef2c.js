108c8bfb3efbca8aec3cc430bec277a625019812
VitaBabenko <110460253+VitaBabenko@users.noreply.github.com>
2023-04-18 21:52:04 +0000

    Deploying to gh-pages from @ VitaBabenko/test-IQ@3a5d570ade6583d082ce2143706303aebf18f03e 🚀

---
+++ index.6971ef2c.js
@@ -0,0 +1,2 @@
+var openBtnMenu=document.querySelector(".nav__menu_btn"),closeBtnMenu=document.querySelector(".mob__menu_close-btn"),menu=document.querySelector(".mob__menu"),toggleMenu=function(){menu.classList.toggle("mob__menu_is-hidden")};openBtnMenu.addEventListener("click",toggleMenu),closeBtnMenu.addEventListener("click",toggleMenu);
+//# sourceMappingURL=index.6971ef2c.js.map
