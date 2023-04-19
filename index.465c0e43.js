2fa8ae8ab45e8f28868ac6c3ac7c9e4c5c3ec589
VitaBabenko <110460253+VitaBabenko@users.noreply.github.com>
2023-04-19 21:41:52 +0000

    Deploying to gh-pages from @ VitaBabenko/test-IQ@ca626dbd3d7ae3dbb4a546011138ef84162f1543 🚀

--- index.465c0e43.js
+++
@@ -1,2 +0,0 @@
-var btnMore=document.querySelector(".section__hero_btn-more"),overlayWrap=document.querySelector(".overlay__wrap"),sectionHero=document.querySelector(".section__hero"),onBtnMore=function(){overlayWrap.classList.remove("visually-hidden"),sectionHero.classList.add("visually-hidden")};btnMore.addEventListener("click",onBtnMore);
-//# sourceMappingURL=index.465c0e43.js.map
