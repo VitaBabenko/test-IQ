5d9263d901ee591a61db6b0069fc032b89bf7ab6
VitaBabenko <babenkovita1988@gmail.com>
2023-04-20 21:08:38 +0300

    in process

--- src/js/test-11.js
+++ src/js/test-11.js
@@ -1,6 +1,7 @@
 const listStar = document.querySelector('.list__wrap_star');
 const link = document.querySelector('.form__link_geometry');
 const wrapTest = document.querySelector('.wrapper__test');
+const wrapResult = document.querySelector('.wrapper__result_test');
 
 const onStarClick = evt => {
   const boxNumber = evt.target;
@@ -17,26 +18,10 @@ const onStarClick = evt => {
 };
 
 const onClickLink = () => {
-  console.log('mmmm');
-  const htmlString = `<div class="wrapper__test">
-        <img
-          src="./images/Group12.png"
-          alt="Загрузка"
-          width="260"
-          class="wrapper__test_img"
-        />
-        <h2 class="wrapper__test_result">Обработка результатов</h2>
-        <div class="preloader__row">
-          <svg class="icon__loader" width="70" height="70">
-            <use href="./images/symbol-defs.svg#icon-spinner"></use>
-          </svg>
-        </div>
-        <p class="wrapper_result">
-          Определение стиля мышления...............
-          ...................................................
-        </p>
-      </div>`;
-  wrapTest.innerHTML = htmlString;
+  wrapTest.classList.add('visually-hidden');
+  wrapResult.classList.remove('visually-hidden');
+
+  setTimeout(() => {});
 };
 
 listStar.addEventListener('click', onStarClick);
