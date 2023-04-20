3a96b5154096be21e6e91c785b83e1459faac245
VitaBabenko <110460253+VitaBabenko@users.noreply.github.com>
2023-04-20 07:19:56 +0000

    Deploying to gh-pages from @ VitaBabenko/test-IQ@efa5515c8cc4c3d12e6bbe83a29d81b2e3f127c7 🚀

---
+++ test_5.f5a9c75f.js
@@ -0,0 +1,2 @@
+var colorContainer=document.querySelector(".form__wrap_color"),onClickBox=function(o){o.target.classList.contains("square-color")&&(console.log(o.target),o.target.classList.add("is-active"))};colorContainer.addEventListener("click",onClickBox);
+//# sourceMappingURL=test_5.f5a9c75f.js.map
